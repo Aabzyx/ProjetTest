292e74efde53b46dcdd97634647a83d3b9be9e1f
Aabzyx <77335648+Aabzyx@users.noreply.github.com>
2021-01-12 14:43:07 +0100

    Update Program.cs

--- ProjetTest/Program.cs
+++ ProjetTest/Program.cs
@@ -7,7 +7,7 @@ namespace ProjetTest
         static void Main(string[] args)
         {
             Console.WriteLine("Aline.");
-            Console.WriteLine("Turbochienne");
+            Console.WriteLine("Turbochienne  ");
             Console.ReadKey();
         }
     }
